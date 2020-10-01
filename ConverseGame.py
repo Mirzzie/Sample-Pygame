29fac94e571649698d03f3cc56cc2077f31c2c81
THE-RARE-CAT <mirswad.chikku@gmail.com>
2020-10-01 22:16:27 +0530

    Code added

---
+++ ConverseGame.py
@@ -0,0 +1,28 @@
+print("[STATUS --> ALPHA]")
+print("Hi there!")
+print("Welcome to the sample pygame!")
+
+ans = input("What's your name?")
+if ans.lower() == "mirzad":
+    print("wow!you are my owner..")
+else:
+    print("That's a sweet name!")
+
+ans = input("Where do you live?")
+if ans.lower() == "kerala,thrissur":
+    print("Ye... I Knew it!")
+else:
+    print("Ooh..Good!")
+
+ans = input("What's your favorite colour?")
+if ans.lower() == "white":
+    print("haha..my owner's fav too!")
+else:
+    print("Good Choice!")
+
+    print("THATS ALL AS OF NOW ..AND MY OWNER IS SOO NOOB TO CODING..")
+    print("BTW...#STAYSAFEFROMCORONA")
+    print("WILL CATCH YOU UP LATER...BYE!!")
+
+
+
